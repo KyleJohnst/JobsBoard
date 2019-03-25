ae75beedf5b79f7c25df000f4715b654a9cd4095
Kyle Johnston <kyle9657@gmail.com>
2019-03-25 16:16:17 +0000

    Adds basic file structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <job-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JobList from './components/JobList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    JobList
   }
 }
 </script>
